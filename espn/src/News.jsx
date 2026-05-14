1acd577d239475a0803c1fb562f4f953dfe826d8
Dakota Kuhn <dakotakuhn27@gmail.com>
2026-05-14 14:17:40 -0400

    Add News component and update dependencies for routing

---
+++ espn/src/News.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+export default function News() {
+  const [news, setNews] = useState("");
+  const link = "https://now.core.api.espn.com/v1/sports/news ";
+}
