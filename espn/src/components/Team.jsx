0cace50673741235d005e43224efe82cda7e7d76
c-o-b-a-x <cobyhughs2008@gmail.com>
2026-05-18 13:31:33 -0400

    stat display updates

--- espn/src/components/Team.jsx
+++ espn/src/components/Team.jsx
@@ -22,38 +22,97 @@ function Team() {
   }, [link]);
 
   if (!team) return <p>Loading...</p>;
+  if (sport === "soccer") {
+    return (
+      <>
+        <div>
+          <h1>{team.displayName}</h1>
+          <img src={team.logos?.[0]?.href} alt={team.displayName} />
 
-  return (
-    <>
-      <div>
-        <h1>{team.displayName}</h1>
-        <img src={team.logos?.[0]?.href} alt={team.displayName} />
+          <h2>Next Event</h2>
+          <p>{team.nextEvent?.[0]?.name}</p>
+          <p>{team.nextEvent?.[0]?.date}</p>
 
-        <h2>Next Event</h2>
-        <p>{team.nextEvent?.[0]?.name}</p>
-        <p>{team.nextEvent?.[0]?.date}</p>
+          <h2>Record</h2>
+          <p>{team.record?.items?.[0]?.summary}</p>
 
-        <h2>Record</h2>
-        <p>{team.record?.items?.[0]?.summary}</p>
+          <h3>Home Record</h3>
+          <p>
+            {team.record?.items?.[0]?.stats?.[22]?.value}-
+            {team.record?.items?.[0]?.stats?.[21]?.value}-
+            {team.record?.items?.[0]?.stats?.[18]?.value}
+          </p>
 
-        <h3>Home Record</h3>
-        <p>
-          {team.record?.items?.[0]?.stats?.[22]?.value}-
-          {team.record?.items?.[0]?.stats?.[21]?.value}-
-          {team.record?.items?.[0]?.stats?.[18]?.value}
-        </p>
+          <h3>Away Record</h3>
+          <p>
+            {team.record?.items?.[0]?.stats?.[15]?.value}-
+            {team.record?.items?.[0]?.stats?.[14]?.value}-
+            {team.record?.items?.[0]?.stats?.[11]?.value}
+          </p>
+        </div>
 
-        <h3>Away Record</h3>
-        <p>
-          {team.record?.items?.[0]?.stats?.[15]?.value}-
-          {team.record?.items?.[0]?.stats?.[14]?.value}-
-          {team.record?.items?.[0]?.stats?.[11]?.value}
-        </p>
-      </div>
+        <Roster />
+      </>
+    );
+  }
+  if (sport === "football") {
+    return (
+      <>
+        <div>
+          <h1>{team.displayName}</h1>
+          <img src={team.logos?.[0]?.href} alt={team.displayName} />
 
-      <Roster />
-    </>
-  );
+          <h2>Next Event</h2>
+          <p>{team.nextEvent?.[0]?.name}</p>
+          <p>{team.nextEvent?.[0]?.date}</p>
+
+          <h2>Record</h2>
+          <p>{team.record?.items?.[0]?.summary}</p>
+
+          <h3>Home Record</h3>
+          <p>
+            {team.record?.items?.[0]?.stats?.[22]?.value}-
+            {team.record?.items?.[0]?.stats?.[21]?.value}-
+            {team.record?.items?.[0]?.stats?.[18]?.value}
+          </p>
+
+          <h3>Away Record</h3>
+          <p>
+            {team.record?.items?.[0]?.stats?.[15]?.value}-
+            {team.record?.items?.[0]?.stats?.[14]?.value}-
+            {team.record?.items?.[0]?.stats?.[11]?.value}
+          </p>
+        </div>
+
+        <Roster />
+      </>
+    );
+  }
+  if (sport === "baseball") {
+    return (
+      <>
+        <div>
+          <h1>{team.displayName}</h1>
+          <img src={team.logos?.[0]?.href} alt={team.displayName} />
+
+          <h2>Next Event</h2>
+          <p>{team.nextEvent?.[0]?.name}</p>
+          <p>{team.nextEvent?.[0]?.date}</p>
+
+          <h2>Record</h2>
+          <p>{team.record?.items?.[0]?.summary}</p>
+
+          <h3>Home Record</h3>
+          <p>{team.record?.items?.[1]?.summary}</p>
+
+          <h3>Away Record</h3>
+          <p>{team.record?.items?.[2]?.summary}</p>
+        </div>
+
+        <Roster />
+      </>
+    );
+  }
 }
 
 export default Team;
